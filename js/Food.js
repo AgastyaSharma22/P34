c7d501820efae02307fc84910faface498cdafe4
AgastyaSharma22 <65062274+AgastyaSharma22@users.noreply.github.com>
2020-09-20 19:05:01 +0530

    Add files via upload

--- js/Food.js
+++ js/Food.js
@@ -5,16 +5,17 @@ class Food{
       this.image=loadImage("images/Milk.png");
     }
     display(){
-      var x=80, y=100;
+      var x=70, y=100;
       imageMode(CENTER);
       image(this.image,720,220,70,70);
       if(this.foodStock!=0){ 
           for(var i=0;i<this.foodStock;i++){ 
               if(i%10==0){ 
-                  x=80; 
+                  x=70; 
                   y=y+50; 
                 } 
-                image(this.image,x,y,50,50); x=x+30; 
+                image(this.image,x,y,50,50);
+                 x=x+30; 
             } 
         }
     }
@@ -31,5 +32,14 @@ class Food{
     }
     getFedTime(lastFed){
       this.lastFed=lastFed;
+    }
+    bedroom(){
+      background(bedroom);
+    }
+    garden(){
+      background(garden);
+    }
+    washroom(){
+      background(washroom);
     }
 }
--- sketch.js
+++ sketch.js
@@ -4,28 +4,39 @@ var database;
 var foodS, foodStock;
 var feed, addFood;
 var lastFed;
+var gameState,readState;
+var bedroom,garden,washroom;
 var foodObj;
+var currentTime;
 function preload(){
   dogImg=loadImage("images/dogImg.png");
   dogImg1=loadImage("images/dogImg1.png");
+  bedroom=loadImage("virtual pet images/Bed Room.png");
+  garden=loadImage("virtual pet images/Garden.png");
+  washroom=loadImage("virtual pet images/Wash Room.png");
 }
 
 function setup() {
   database = firebase.database();
-  createCanvas(1000,400);
+  createCanvas(400,500);
   foodObj=new Food();
-  dog=createSprite(800,200,100,100);
+  dog=createSprite(200,400,150,150);
   dog.addImage(dogImg);  
   dog.scale=0.15;
   feed=createButton("Feed the dog");
-  feed.position(700,95);
+  feed.position(500,95);
   feed.mousePressed(feedDog);
   addFood=createButton("Add Food"); 
-  addFood.position(800,95); 
+  addFood.position(600,95); 
   addFood.mousePressed(addFoods);
   
   foodStock=database.ref('Food');
   foodStock.on("value",readStock);
+
+  readState=database.ref('gameState');
+  readState.on("value",function(data){
+    gameState=data.val();
+  });
 }
 
 
@@ -39,10 +50,25 @@ function draw() {
 fill(255,255,254); 
 textSize(15); 
 if(lastFed>=12){
-   text("Last Feed : "+ lastFed%12 + " PM", 350,30); 
+   text("Last Feed : "+ lastFed%12 + " PM", 100,30); 
   }else if(lastFed==0){
      text("Last Feed : 12 AM",350,30); }
-     else{ text("Last Feed : "+ lastFed + " AM", 350,30); } drawSprites();
+     else{ text("Last Feed : "+ lastFed + " AM", 100,30); } drawSprites();
+
+     currentTime=hour();
+  if(currentTime==(lastFed+1)){
+    update("Playing");
+    foodObj.garden();
+  }else if(currentTime==lastFed+2){
+    update("Sleeping");
+    foodObj.bedroom();
+  }else if(currentTime==(lastFed+2) && currentTime<=(lastFed+4)){
+    update("Bathing");
+    foodObj.washroom();
+  }else{
+    update("Hungry");
+    foodObj.display();
+  }
 }
 function readStock(data){
   foodS=data.val();
@@ -60,4 +86,9 @@ function addFoods(){
   foodS++; database.ref('/').update({
     Food:foodS 
   }) 
+}
+function update(state){
+  database.ref('/').update({
+    gameState:state
+  });
 }
